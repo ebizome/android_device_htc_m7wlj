53ebf07fdba6ff31141820257b2128f1e12dfb91
5x <droidfivex@gmail.com>
2016-12-30 21:24:21 +0900

    m7wlj: initial commit
    based on m7vzw implement
    
    Change-Id: If84deeee15a1b0b1df5c5e306ec69a6e3db4fed1

---
+++ cm.mk
@@ -0,0 +1,31 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/htc/m7wlj/full_m7wlj.mk)
+
+# Inherit CM full phone configuration
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Device naming
+PRODUCT_NAME := cm_m7wlj
+
+# Override build props
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_ID=KOT49H \
+    BUILD_FINGERPRINT="htc/m7_jp_kdi/m7wlj:4.4.2/KOT49H/363468.12:user/release-keys" \
+    PRIVATE_BUILD_DESC="3.13.970.12 CL363468 release-keys" \
+    PRODUCT_NAME=m7wlj
+
+# Override ro.com.google.clientidbase
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
